04019c289a540509dc4e4fdacd2b00179892bff8
Brandon <brandonr717@nycstudents.net>
2024-11-10 01:42:51 -0500

    Finished project

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,21 +3,68 @@ public class LinearEquation {
 
 
 
-    private String cord1;
-    private String cord2;
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    private double slope;
+    private String yIntercept;
+    private String distance;
 
 
-
-
-    public LinearEquation(String cord1, String cord2) {
-        this.cord1 = cord1;
-        this.cord2 = cord2;
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+       this.x1 = x1;
+       this.y1 = y1;
+       this.x2 = x2;
+       this.y2 = y2;
+       slope = ((double) y2 - y1) / ((double) x2 - x1);
     }
 
+
+
+
+
+    public void distance() {
+        int a = x2 - x1;
+        int b = y2 - y1;
+        double c = Math.sqrt((a * a) + (b * b));
+    }
+
+    public void yIntercept() {
+        double slope = (y2 - y1) / (x2 - x1);
+        double val = y1 * slope;
+        if (val < 0) {
+            double yIntercept = y2 + (val + (val * -2));
+        } else {
+            double yIntercept = y2 + (val - (val * 2));
+        }
+    }
+
+    public void slope() {
+        double slope = (y2 - y1) / (x2 - x1);
+
+    }
+
+    public void equation() {
+        String equation  = "y = " + slope + "x" + yIntercept;
+    }
+
+    public String coordinateForX(double x) {
+        String y = (slope * x) + yIntercept;
+        return "Coordinate for x: (" + x + "," + y + ")";
+    }
 
     public String lineInfo() {
+        System.out.println("The two points are: (" +x1+", "+ y1 + ") and " + "(" + x2 + ", " + y2 + ")");
+        if (slope == 0) {
+            System.out.println("The equation of the line between these points is: y = " + yIntercept);
+        } else {
+            System.out.println("The equation of the line between these points is: y = " + slope + "x " + "+ " + yIntercept);
+        }
+        System.out.println("The equation of a line is: " + yIntercept);
+        System.out.println("The slope of the line is: " + slope);
+        System.out.println("The distance between these points is " + distance);
         return "";
+
     }
 }
-
-
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -38,13 +38,58 @@ public class LinearEquationLogic {
         System.out.println("Enter coordinate 2: ");
         String cord2 = myScanner.nextLine();
         commaLocation = cord2.indexOf(",");
-        String x2 = cord1.substring(1, commaLocation);
+        String x2 = cord2.substring(1, commaLocation);
         int x2Int = Integer.parseInt(x2);
         endParenthesis = cord2.indexOf(")");
-        String y2 = cord1.substring((commaLocation + 2), endParenthesis);
+        String y2 = cord2.substring((commaLocation + 2), endParenthesis);
         int y2Int = Integer.parseInt(y2);
 
+        linearEquation = new LinearEquation(x1Int, y1Int, x2Int, y2Int);
 
-        linearEquation = new LinearEquation(cord1, cord2);
+        System.out.println(linearEquation.lineInfo());
+
+        System.out.println("Enter an x value: ");
+        double x = myScanner.nextDouble();
+
+        System.out.println(linearEquation.coordinateForX(x));
+
+        System.out.println("Do you want to repeat(y/n): ");
+        String ans = myScanner.nextLine();
+        while (ans == "y") {
+            System.out.println("Welcome to the linear equation calculator");
+            System.out.println("Enter coordinate 1: ");
+            cord1 = myScanner.nextLine();
+            commaLocation = cord1.indexOf(",");
+            x1 = cord1.substring(1, commaLocation);
+            x1Int = Integer.parseInt(x1);
+            endParenthesis = cord1.indexOf(")");
+            y1 = cord1.substring((commaLocation + 2), endParenthesis);
+            y1Int = Integer.parseInt(y1);
+            System.out.println("Enter coordinate 2: ");
+            cord2 = myScanner.nextLine();
+            commaLocation = cord2.indexOf(",");
+            x2 = cord2.substring(1, commaLocation);
+            x2Int = Integer.parseInt(x2);
+            endParenthesis = cord2.indexOf(")");
+            y2 = cord2.substring((commaLocation + 2), endParenthesis);
+            y2Int = Integer.parseInt(y2);
+
+            linearEquation = new LinearEquation(x1Int, y1Int, x2Int, y2Int);
+
+            if (x2Int == x1Int) {
+                System.out.println("Equation is undefined because the points fall on a vertical line");
+            }
+
+            System.out.println(linearEquation.lineInfo());
+
+            System.out.println("Enter an x value: ");
+            x = myScanner.nextDouble();
+
+            System.out.println(linearEquation.coordinateForX(x));
+            System.out.println("Do you want to repeat(y/n): ");
+            ans = myScanner.nextLine();
+        }
+
+        System.out.println("End");
     }
 }
